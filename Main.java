d170a768d3371d83ab2c1998fb5aa6533e760152
Josué Elias <josue.elias.f@gmail.com>
2023-11-19 17:41:29 -0300

    Board is rendering

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
 import java.io.IOException;
 import java.util.Scanner;
 
+import Board.Board;
+
 public class Main {
     public static void main(String... arg) throws IOException, InterruptedException {
 
-        final int BOARD_WIDTH = 8, BOARD_HEIGHT = 8;
+        Board playBoard = new Board();
 
-        String[][] board =  new String[BOARD_HEIGHT][BOARD_WIDTH];
-        String[][] displayBoard = new String[BOARD_HEIGHT * 2][BOARD_WIDTH * 2];
-        // String[][] board =  new String[24][24];
         Scanner input = new Scanner(System.in);
 
         boolean isPlaying = true;
@@ -17,18 +16,30 @@ public class Main {
 
         while (isPlaying){
 
-            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            printBorad(board);
+            //Comando para limpar a tela
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor(); 
+
+            playBoard.printBoard();
 
             System.out.print("Digite o seu comando: ");
 
             String command = input.nextLine();
 
-            if (command.equals("q") ){
-                isPlaying = false;
-                break;
-            } else {
-                board[4][4] = command;
+            switch (command) {
+                case "q":
+                    isPlaying = false;                    
+                    break;
+                
+                case "su":
+                    playBoard.setScaleFactor(false);
+                    break;
+
+                case "sd":
+                    playBoard.setScaleFactor(true);
+                    break;
+            
+                default:
+                    break;
             }
 
         }
@@ -45,34 +56,17 @@ public class Main {
         int board_x = board.length, board_y = board[board_x].length;
         String[][] displayBoard =  new String[board_x * newSizeMult][board_y * newSizeMult];
         
-
-
-    }
-
-    static void printBorad(String[][] board) {
-        for (int i = 0; i < board.length; i++){
-            boolean startWhite = i % 2 == 0;
-            for (int j = 0; j < board[i].length; j++){
-                if (board[i][j] == null) {
-                    if (startWhite) {
-                        if (j % 2 == 0) {
-                            board[i][j] = "██";
-
-                        } else {
-                            board[i][j] = "▓▓";
-                        } 
-                    } else {
-                        if (j % 2 != 0) {
-                            board[i][j] = "██";
-                        } else {
-                            board[i][j] = "▓▓";
-                        } 
-                    }
-                } 
-                System.out.print(board[i][j]);
+        for (int i = 0; i < board.length; i++)
+        {
+            for ( int j = 0; j < board[i].length; j++)
+            {
+                displayBoard[i][j + 3] = "A";
+                
             }
-            System.out.print("\n");
         }
 
+
     }
+
+
 }
---
+++ Piece/Piece.java
@@ -0,0 +1,6 @@
+package Piece;
+
+public class Piece {
+    
+    
+}
